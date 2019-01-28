de85a0263a3fc046f644779737ac45689e50e4f6
Patryk D. Cichy <patryk.d.cichy@gmail.com>
2019-01-28 10:04:25 +0100

    Handle new usbmon implementation
    On newest Fedora (>= 29) and Ubuntu you get `Operation not permitted` errors. 
    The `usbmon` package is needed. You access the usb debug logs via the `usbmon` command

--- lenovo_keyboard.sh
+++ lenovo_keyboard.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 modprobe usbmon
-stdbuf -i0 -o0 -e0 grep -h -Po '\K[49]* = [0-9a-f ]*71$' /sys/kernel/debug/usb/usbmon/*u | stdbuf -i0 -o0 -e0 tr -d ' ' | /usr/local/bin/kbdusb
+if which usbmon; then
+   stdbuf -i0 -o0 -e0 usbmon -i 1 -f0 | stdbuf -i0 -o0 -e0 grep -h -Po '\K[49]* = [0-9a-f ]*71$' | stdbuf -i0 -o0 -e0 tr -d ' ' | /usr/local/bin/kbdusb
+else
+   stdbuf -i0 -o0 -e0 grep -h -Po '\K[49]* = [0-9a-f ]*71$' /sys/kernel/debug/usb/usbmon/*u | stdbuf -i0 -o0 -e0 tr -d ' ' | /usr/local/bin/kbdusb
+fi
